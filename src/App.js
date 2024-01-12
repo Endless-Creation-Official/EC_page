e96ed1555b395e93f5b1a7d0b0345b276dfcdb7a
JiinHong <com5942@seoultech.ac.kr>
2024-01-12 18:33:07 +0900

    add introduction

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 
-function Header(){
+function Header() {
   return (
   <header id="main-header" >
     <div class="header--container max-width-1200">
@@ -17,12 +17,38 @@ function Header(){
   );
 }
 
-function Main(){
+function Main() {
   return (
     <div class="main-title">
-      <h1>성장하는 즐거움</h1>
+      <h1>성장하는 즐거움!</h1>
       <div id="element"><h1>Endless Creation</h1></div>
       <h2 class="text-box">34기 모집 예정!</h2>
+    </div>
+  )
+}
+
+function AboutEC() {
+  return (
+    <div class="center-text">
+      <h1>About EC</h1>
+      <div class="container">
+        <div class="box">
+          <span>설립일</span>
+          Since 1991
+        </div>
+        <div class="box">
+          <span>멤버</span>
+          <div>
+            46 명
+          </div>
+        </div>
+        <div class="box">
+          <span>누적 프로젝트 수</span>
+          <div>
+            84 <b>+</b>
+          </div> 
+        </div>
+      </div>
     </div>
   )
 }
@@ -32,21 +58,11 @@ function Recruiting() {
     <div class="recruiting">
       <h2 class="center-text">Recruiting Schedule</h2>
       <ul>
-        <li>
-          34기 서류 지원
-        </li>
-        <li>
-          1차 합격 발표
-        </li>
-        <li>
-          2차 면접
-        </li>
-        <li>
-          최종 합격 발표
-        </li>
-        <li>
-          위크샵
-        </li>
+        <li>34기 서류 지원</li>
+        <li>1차 합격 발표</li>
+        <li>2차 면접</li>
+        <li>최종 합격 발표</li>
+        <li>워크샵</li>
       </ul>
     </div>
   )
@@ -57,6 +73,7 @@ function App() {
     <div>
       <Header></Header>
       <Main></Main>
+      <AboutEC></AboutEC>
       <Recruiting></Recruiting>
     </div>
   );
