05421961e720edae73ab74e36e4e5877e84613eb
JiinHong <com5942@seoultech.ac.kr>
2024-01-20 16:24:19 +0900

    route problem solve

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home';
 import Aboutus from './components/Aboutus/Aboutus';
 import Members from './components/Members/Members';
 import Recruit from './components/Recruit/Recruit';
+import Apply from './components/Apply/Apply';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path='/Aboutus' element={<Aboutus />}></Route>
         <Route path='/Members' element={<Members />}></Route>
         <Route path='/Recruit' element={<Recruit />}></Route>
+        <Route path="/apply" element={<Apply />}></Route>
       </Routes>
     </Router>
   );
