60598eb276e0d919a426a91cdbe60e135cd57dfe
경태의LAPTOP\kimgt <kimgt2828@gmail.com>
2024-01-20 14:31:39 +0900

    add Apply file

---
+++ src/components/Apply/Apply.js
@@ -0,0 +1,15 @@
+//Apply.js
+import React from "react";
+import Header from "../Header/Header";
+import style from "./Apply.muodle.css";
+
+function Main(){
+
+}
+
+function Apply() {
+    return <div>
+        <main></main>
+        <Header></Header>
+    </div>
+}
