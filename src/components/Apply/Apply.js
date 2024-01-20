f6318acf3e8797a93f9224073be11ffda240a23b
경태의LAPTOP\kimgt <kimgt2828@gmail.com>
2024-01-20 15:36:47 +0900

    test

---
+++ src/components/Apply/Apply.js
@@ -0,0 +1,24 @@
+//Apply.js
+import React from "react";
+import Header from "../Header/Header";
+import styles from "./Apply.moudle.css";
+
+function Main() {
+    return (
+    <div className={styles.main}>
+        <h1>ㅎㅇ</h1>
+    </div>
+    )
+}
+
+function Apply() {
+
+    return( 
+    <div>
+        <Header></Header>
+        <Main>ㅇㅇ</Main>
+    </div>
+    )
+}
+
+export default Apply;
