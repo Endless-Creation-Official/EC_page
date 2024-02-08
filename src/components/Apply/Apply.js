12fef8c5f9e8854556ea66decf34ba109e07e8fd
JiinHong <com5942@seoultech.ac.kr>
2024-02-08 23:55:20 +0900

    change apply page design

--- src/components/Apply/Apply.js
+++ src/components/Apply/Apply.js
@@ -6,6 +6,9 @@ import axios from 'axios';
 function Main() {
   // form state
   const [name, setName] = useState('');
+  const [major, setMajor] = useState('');
+  const [studentid, setStudentid] = useState('');
+  const [birth, setBirth] = useState('');
   const [phonenumber, setPhoneNumber] = useState('');
   const [email, setEmail] = useState('');
   const [q1, setQ1] = useState('');
@@ -18,6 +21,9 @@ function Main() {
     // console.log(name, phonenumber, email);
     const data = {
       Name: name,
+      Major: major,
+      Studentid: studentid,
+      Birth: birth,
       Phonenumber: phonenumber,
       Email: email,
       Q1: q1,
@@ -28,6 +34,9 @@ function Main() {
       console.log(response);
       // clearing form fields
       setName('');
+      setMajor('');
+      setStudentid('');
+      setBirth('');
       setPhoneNumber('');
       setEmail('');
       setQ1('');
@@ -43,59 +52,64 @@ function Main() {
   return (
     <form onSubmit={handleSubmit} method="post">
       <div className={styles.main}>
-        <h1 className={styles.title}>34기 지원서 </h1>
+        <h1 className={styles.title}>EC 34기 지원서 </h1>
         <h2>개인 정보</h2>
-          이름
-          <input className={styles.privacy} name="name" type="text" placeholder="이름을 입력해주세요"
-            onChange={(e) => setName(e.target.value)} value={name}></input>
-          
-          전화 번호
-          <input className={styles.privacy} name="phoneNumber" type="text" placeholder="전화번호를 입력해주세요"
-            onChange={(e) => setPhoneNumber(e.target.value)} value={phonenumber}></input>
+        이름
+        <input className={styles.privacy} name="name" type="text" placeholder="이름을 입력해주세요"
+          onChange={(e) => setName(e.target.value)} value={name}></input>
 
-          Email
-          <input className={styles.privacy} name="email" type="text" placeholder="이메일을 입력해주세요"
-            onChange={(e) => setEmail(e.target.value)} value={email}></input>
+        소속학과
+        <input className={styles.privacy} name="major" type="text" placeholder="소속학과를 입력해주세요"
+          onChange={(e) => setMajor(e.target.value)} value={major}></input>
+
+        학번
+        <input className={styles.privacy} name="studentid" type="text" placeholder="학번을 입력해주세요"
+          onChange={(e) => setStudentid(e.target.value)} value={studentid}></input>
+
+        생년월일
+        <input className={styles.privacy} name="birth" type="text" placeholder="생년월일을 입력해주세요 ex) 2001-03-19"
+          onChange={(e) => setBirth(e.target.value)} value={birth}></input>
+
+        전화번호
+        <input className={styles.privacy} name="phoneNumber" type="text" placeholder="전화번호를 입력해주세요 ex) 010-1234-1234"
+          onChange={(e) => setPhoneNumber(e.target.value)} value={phonenumber}></input>
+
+        Email
+        <input className={styles.privacy} name="email" type="text" placeholder="이메일을 입력해주세요"
+          onChange={(e) => setEmail(e.target.value)} value={email}></input>
 
         <h2>질문 목록</h2>
-        <div className={styles.form}>
-          <div className={styles.question}>
-            <h1 className={styles.question}>{`질문 1`}</h1>
-            <p>간단한 자기소개와 지원동기를 작성해주세요!</p>
-          </div>
-          <textarea type="text"
-            name="q1"
-            className={styles.textbox}
-            placeholder="내용을 입력해주세요"
-            onChange={(e) => setQ1(e.target.value)} value={q1}>
-          </textarea>
+
+        <div className={styles.question}>
+          <p>1. 간단한 자기소개와 지원동기를 작성해주세요!</p>
         </div>
-        <div className={styles.form}>
-          <div className={styles.question}>
-            <h1 className={styles.question}>{`질문 2`}</h1>
-            <p>살면서 어떠한 문제를 해결한 경험이 있나요? 그 문제를 어떻게 해결했는지, 이를 통해 무엇을 배웠고 느꼈는지 구체적으로 설명해주세요! 사소한 경험이라도 괜찮습니다.</p>
-          </div>
-          <textarea type="text"
-            name="q2"
-            className={styles.textbox}
-            placeholder="내용을 입력해주세요"
-            onChange={(e) => setQ2(e.target.value)} value={q2}>
-          </textarea>
+        <textarea type="text"
+          name="q1"
+          className={styles.textbox}
+          placeholder="내용을 입력해주세요"
+          onChange={(e) => setQ1(e.target.value)} value={q1}>
+        </textarea>
+
+        <div className={styles.question}>
+          <p>2. 살면서 어떠한 문제를 해결한 경험이 있나요? 그 문제를 어떻게 해결했는지, 이를 통해 무엇을 배웠고 느꼈는지 구체적으로 설명해주세요! 사소한 경험이라도 괜찮습니다.</p>
         </div>
-        <div className={styles.form}>
-          <div className={styles.question}>
-            <h1 className={styles.question}>{`질문 3`}</h1>
-            <p>만들고 싶거나 관심있는 웹서비스, 혹은 하고싶은 스터디를 작성해주세요!</p>
-          </div>
-          <textarea type="text"
-            name="q3"
-            className={styles.textbox}
-            placeholder="내용을 입력해주세요"
-            onChange={(e) => setQ3(e.target.value)} value={q3}>
-          </textarea>
+        <textarea type="text"
+          name="q2"
+          className={styles.textbox}
+          placeholder="내용을 입력해주세요"
+          onChange={(e) => setQ2(e.target.value)} value={q2}>
+        </textarea>
+
+        <div className={styles.question}>
+          <p>3. 만들고 싶거나 관심있는 웹서비스, 혹은 하고싶은 스터디를 작성해주세요!</p>
         </div>
-      </div>
-      <div className={styles.ButtonContainer}>
+        <textarea type="text"
+          name="q3"
+          className={styles.textbox}
+          placeholder="내용을 입력해주세요"
+          onChange={(e) => setQ3(e.target.value)} value={q3}>
+        </textarea>
+
         <button type="submit" className={styles.admitButton}>
           제출하기
         </button>
