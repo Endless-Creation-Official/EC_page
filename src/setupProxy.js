2a5176a1bdf87f1bbbb7fde72d251092b10d1687
STkangyh <k22101149@g.seoultech.ac.kr>
2024-07-15 21:19:42 +0900

    fix: proxy 설정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080',	// 서버 URL or localhost:설정한포트번호
+      target: 'http://3.39.224.2:8080',	// 서버 URL or localhost:설정한포트번호
       changeOrigin: true,
     })
   );
