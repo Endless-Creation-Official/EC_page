66ab1bcb0bcdd64bed14c0da74b599f3e3b04662
KKT <kimgt2828@gmail.com>
2024-07-17 15:32:52 +0900

    롤백

--- src/components/Apply/Apply.jsx
+++ src/components/Apply/Apply.jsx
@@ -60,7 +60,7 @@ function Main() {
     const jsonData = JSON.stringify(formData);
 
     axios
-      .post('${PROXY}/applyForm', jsonData, {
+      .post('/api/applyForm', jsonData, {
         headers: {
           'Content-Type': 'application/json',
         },
