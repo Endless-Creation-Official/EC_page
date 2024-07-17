a3c73cc665c15feee1fda953e093766c9c1d581e
Gyeongtae Kim <127713989+kimgt0128@users.noreply.github.com>
2024-07-17 10:17:48 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     '/api',
     createProxyMiddleware({
       target: 'http://43.202.40.240:8080',	// 서버 URL or localhost:설정한포트번호
-      changeOrigin: true,
+      changeOrigin: true
     })
   );
 };  
