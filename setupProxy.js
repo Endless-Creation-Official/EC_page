ce3815446d178aba3c33541499d526a69d0b4b28
KKT <kimgt2828@gmail.com>
2024-07-19 01:15:19 +0900

    setUpProxy.js 위치 변경

--- setupProxy.js
+++ setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/proxy',
     createProxyMiddleware({
-      target: 'http://43.202.40.240:8080',	// 서버 URL or localhost:설정한포트번호
+      target: 'https://43.202.40.240:8080',	// 서버 URL or localhost:설정한포트번호
       changeOrigin: true
     })
   );
