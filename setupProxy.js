8fb16295e89d10715b96f8b5e816a8d7be077e04
KKT <kimgt2828@gmail.com>
2024-07-19 01:27:27 +0900

    proxy -> api로 변경

--- setupProxy.js
+++ setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/proxy',
+    '/api',
     createProxyMiddleware({
       target: 'https://43.202.40.240:8080',	// 서버 URL or localhost:설정한포트번호
       changeOrigin: true
