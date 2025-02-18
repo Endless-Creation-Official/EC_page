04d93b62e139e948bee8d9d933a2d1dcab7156ca
JiinHong <com5942@seoultech.ac.kr>
2025-02-19 01:02:48 +0900

    35기 일정

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -327,24 +327,28 @@ function Process() {
         </colgroup>
         <tbody>
           <tr>
-            <td>8.5 ~ 8.24</td>
-            <td>34기 서류 지원</td>
+            <td> ~ 3.9</td>
+            <td>35기 서류 지원</td>
           </tr>
           <tr>
-            <td>8.25</td>
+            <td>3.10</td>
             <td>1차 합격 발표</td>
           </tr>
           <tr>
-            <td>8.26 ~ 8.31</td>
+            <td>3.10 ~ 3.12</td>
             <td>2차 면접(대면)</td>
           </tr>
           <tr>
-            <td>9.1</td>
+            <td>3.13</td>
             <td>최종 합격 발표</td>
           </tr>
           <tr>
-            <td>9.6</td>
+            <td>3.14</td>
             <td>개강총회</td>
+          </tr>
+          <tr>
+            <td>5.3 ~ 5.4</td>
+            <td>MT</td>
           </tr>
         </tbody>
       </table>
