4f3826ad431a5869d109f98d8e8233770941ab71
JiinHong <com5942@seoultech.ac.kr>
2025-02-20 13:37:00 +0900

    recruit 버튼 활성화

--- src/components/Recruit/Recruit.jsx
+++ src/components/Recruit/Recruit.jsx
@@ -12,7 +12,7 @@ function Main() {
       <div className={styles.firstContainer}>
         <h1>EC와 함께하고 싶다면?</h1>
         <Link to='/apply'>
-          <button className={styles.button} onClick={onClickOverTime}>
+          <button className={styles.button}>
             지원하러 가기
           </button>
         </Link>
