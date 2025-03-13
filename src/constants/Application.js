21ae6ee2ff44dc964ec2c57314f91de3ff3cd0b8
Younghoon <yhkang2003@gmail.com>
2025-03-13 11:35:18 +0900

    feat: add application configuration constants for open period and messages

---
+++ src/constants/Application.js
@@ -0,0 +1,10 @@
+export const APPLICATION_CONFIG = {
+    isOpen: false,
+    period: {
+        start: '2025-02-20',
+        end: '2025-03-08'
+    },
+    message: {
+        closed: '지원 기간이 아닙니다.',
+    }
+};
